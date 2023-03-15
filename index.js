f6ef0be658242374edb03e425716e7155127504e
nguyendong47 <dongnguyenvan.it@gmail.com>
2023-03-15 20:28:25 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
 const middleware = jsonServer.defaults();
@@ -6,5 +7,6 @@ const port = process.env.PORT || 3000;
 
 server.use(middleware);
 server.use(router);
+server.use(cors);
 
 server.listen(port);
